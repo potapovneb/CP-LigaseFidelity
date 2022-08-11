2d7f7de1d806d270ce242ac2a51390101b50e9b6
Vladimir Potapov <potapov@neb.com>
2022-08-11 13:59:03 -0400

    updated README file with workflow details

--- README.md
+++ README.md
@@ -18,98 +18,223 @@ The computational workflow requires a number of tools to be installed and availa
 
 ## Custom PERL scripts
 
-These custom scripts are provided as part of this GitHub repository. The scripts must be made available from the command line in your system (for example, by adding the scripts directory to ```$PATH``` environment variable).
+These custom scripts are provided as part of Ligase Fidelity GitHub repository. The scripts must be made available from the command line in your system (for example, by adding the scripts directory to ```$PATH``` environment variable).
 
 * ```cluster.pl``` - Identify top/bottom subreads in PacBio sequencing reads.
 * ```split.pl``` - Split top/bottom subreads to separate files.
-* ```extract.pl``` - ???Extract overhangs and barcodes.
-* ```bam2csv.pl``` - ???
-* ```reporter.pl``` - ???
+* ```extract.pl``` - Extract fragments of sequenced amplicons.
+* ```bam2csv.pl``` - Extract stats for PacBio sequencing reads.
+* ```reporter.pl``` - Output overhang and barcode sequences.
 * ```mktable_barcode.pl``` - Compute nucleotide frequencies in barcodes.
 * ```mktable.pl``` - Tabulate ligase fidelity results.
 
 # Computational workflow
 
+Processing PacBio sequencing data in the Ligase Fidelity computational workflow proceeds through a series of steps, where output of one step serves as input for the next step. As a result, this workflow produces a set of tables summarizing ligase fidelity data.
+
+The ```scripts/``` directory contains an example workflow (```workflow.sh```) that can be executed from the command line. Before executing the example workflow, make sure that the ```$LIGASE_FIDELITY_DIR``` environment variable in ```workflow.sh``` points to the Ligase Fidelity GitHub repository on your computer.
+
+The computational steps are described below.
+
+## Example PacBio BAM sequencing data
+
+For the purpose of this example, you can download PacBio sequencing data for the b4 substrate sequencing.
+
+```
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra70/SRZ/019043/SRR19043774/lib26.bam
+```
+
+For convenience, we define a set of environment variables to define location of the input data files and the output directory. Please update these variables according to your computational environment.
+
+Input PacBio BAM subreads file:
+```
+subreads=$PWD/movie.subreads.bam
+```
+
+Reference FASTA file:
+```
+reference=$LIGASE_FIDELITY_DIR/references/b4.fasta
+```
+
+Output directory:
+```
+rundir=$PWD/ligase_fidelity_output
+```
+
+This repository comes with the reference FASTA files for the 4-base overhang substrate (```b4.fasta```).
+
+## Cluster and split top/bottom strands
+
+In this step, ```cluster.pl``` script detects a continuous stretch of insert sequences separated by adapter sequences (based on expected lengths of insert and adapter sequences) in PacBio long polymerase reads. Then ```split.pl``` script splits detected insert sequences to two separate groups.
+
+```
+### create output directory for clustered reads
+jobdir=$rundir/01-cluster
+mkdir -p $jobdir
+cd $jobdir
+
+echo "cluster.pl"
+time cluster.pl --insert_length 98 ${subreads} | bzip2 - > clusters.csv.bz2
+
+echo "split.pl"
+time split.pl ${subreads} clusters.csv.bz2
+
+echo "samtools (1)"
+time samtools view -Sb subreads.fwd.sam > subreads.fwd.bam
+
+echo "samtools (1)"
+time samtools view -Sb subreads.rev.sam > subreads.rev.bam
+```
+
+## Build CCS sequences
+
+PacBio ```ccs``` tool builds consensus sequences for "top" and "bottom" strands for each sequenced product.
+
+```
+### create output directory for consensus reads
+jobdir=$rundir/02-ccs
+mkdir -p $jobdir
+cd $jobdir
+
+echo "ccs (1)"
+time ccs \
+    --report-file=subreads_ccs.fwd.csv \
+    --log-file=subreads_ccs.fwd.log \
+    --num-threads=8 \
+    --min-passes=1 \
+    $rundir/01-cluster/subreads.fwd.bam subreads_ccs.fwd.bam
+
+echo "ccs (2)"
+time ccs \
+    --report-file=subreads_ccs.rev.csv \
+    --log-file=subreads_ccs.rev.log \
+    --num-threads=8 \
+    --min-passes=1 \
+    $rundir/01-cluster/subreads.rev.bam subreads_ccs.rev.bam
+```
+
+## Map CCS sequences
+
+Consensus sequences are mapped to the expected reference using PacBio ```pbmm2``` tool.
+
+```
+### create output directory for mapped consensus reads
+jobdir=$rundir/03-mapping
+mkdir -p $jobdir
+cd $jobdir
+
+echo "pbmm2 (1)"
+time TMPDIR=$PWD pbmm2 align $reference $rundir/02-ccs/subreads_ccs.fwd.bam aligned_reads.fwd.bam \
+    --sort \
+    --min-concordance-perc 75.0 \
+    --sample \"\" \
+    --num-threads 16 \
+    --log-level TRACE \
+    --log-file aligned_reads.fwd.log
+
+echo "pbmm2 (2)"
+time TMPDIR=$PWD pbmm2 align $reference $rundir/02-ccs/subreads_ccs.rev.bam aligned_reads.rev.bam \
+    --sort \
+    --min-concordance-perc 75.0 \
+    --sample \"\" \
+    --num-threads 16 \
+    --log-level TRACE \
+    --log-file aligned_reads.rev.log
+```
+
+## Fragment CCS sequences
+
+Parts of the top and bottom strands corresponding to overhangs and internal barcode regions are detected using ```extract.pl``` script. See insert structure below for details.
+
+```
+### create output directory for fragmented consensus reads
+jobdir=$rundir/04-fragments
+mkdir -p $jobdir
+cd $jobdir
+
+echo "extract.pl (1)"
+time extract.pl \
+    --region 1-3,3-10/1,10-12 \
+    --region 45-47,47-52/1,52-54 \
+    --region 87-89,89-96/1,96-98 \
+    $rundir/03-mapping/aligned_reads.fwd.bam $reference >fragments.fwd.csv 2>fragments.fwd.log
+
+echo "extract.pl (2)"
+time extract.pl \
+    --region 1-3,3-10/1,10-12 \
+    --region 45-47,47-52/1,52-54 \
+    --region 87-89,89-96/1,96-98 \
+    $rundir/03-mapping/aligned_reads.rev.bam $reference >fragments.rev.csv 2>fragments.rev.log
+
+echo "bam2csv.pl (1)"
+time bam2csv.pl $rundir/02-ccs/subreads_ccs.fwd.bam zmws.fwd.csv
+
+echo "bam2csv.pl (2)"
+time bam2csv.pl $rundir/02-ccs/subreads_ccs.rev.bam zmws.rev.csv
+```
+
+## Raw counts
+
+The resulting fragment files produced in the previous step are filtered and all unique overhang (and barcode) sequences are tabulated.
+
+```
+### create output directory for overhang counts
+jobdir=$rundir/05-results
+mkdir -p $jobdir
+cd $jobdir
+
+echo "reporter.pl"
+time reporter.pl \
+    --etype b4 \
+    --match 5 \
+    --np 5 \
+    --bcout barcodes.csv \
+    --ohout overhangs.csv \
+    $rundir/04-fragments
+```
+
+## Summary tables
+
+The tabulated overhang (and barcode) sequences are processed to generate a set of output tables. See interpreation of results section below.
+
+```
+### create output directory for summary tables
+jobdir=$rundir/06-summary
+mkdir -p $jobdir
+cd $jobdir
+
+echo "mktable_barcode.pl"
+mktable_barcode.pl $rundir/05-results/barcodes.csv > table-barcode_base_frequencies.csv
+
+echo "mktable.pl"
+mktable.pl --size 4 --prefix table $rundir/05-results/overhangs.csv
+```
+
+# Insert structure of the b4 substrate
+
+The double stranded insert for the b4 substrate is schematically presented below. The Ligase Fidelity scripts are aimed at extracting the overhang and barcode sequences for each sequenced product. This is achieved by aligning consensus sequences of both top and bottom strands to the expected reference sequence and extracting sequence fragments corresponding to overhangs and barcodes. Additionally, the scripts extract the constant regions immediately adjacent to the overhang and barcode regions to ensure correcteness of the mapped consensus sequences.
+
+```
+         1         2         3         4         5         6         7         8         9
+12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678
+TTGNNNNNNCGTTGATCAATGGACGGCGCACTGGATCGCAGGTCTCCNNNNGGAGACCTGCGATCCAGTGCGCCGTCCATTGATCAACGNNNNNNCAA
+---      ---                                ---    ---                                ---      ---
+
+1..3    left constant region (1)
+4..9    left internal barcode region
+10..12  left constant region (2)
+
+45..47  left constant region before overhang
+48..51  overhang
+52..54  right constant region after overhang
+
+87..89  right constant region (1)
+90..95  right internal barcode region
+96..98  right constant region (2)
+```
+
+# Interpretation of results
+
 # Citing
 
 * Potapov V, Ong JL, Kucera RB, Langhorst BW, Bilotti K, Pryor JM, Cantor EJ, Canton B, Knight TF, Evans Jr TC, and Lohman GJS (2018). Comprehensive Profiling of Four Base Overhang Ligation Fidelity by T4 DNA Ligase and Application to DNA Assembly. *ACS Synth. Biol.* 7(11):2665–2674. https://doi.org/10.1021/acssynbio.8b00333
-
-<!-- 
-
-
-# Dependencies
-
-## Global environments
-```
-conda activate pbbioconda
-module load samtools-1.3.1
-```
-
-```
-qsub \
-        -v root="$PROJDIR",rundir="$rundir",rname="$reference",collectionPathUri="$collectionPathUri",instrument="SEQUEL" \
-        -N "demux$sampleId" \
-        -o "$rundir"/workflow.log \
-        -j yes \
-        "$PROJDIR"/scripts/workflow-demux.sh
-```
-
-## Perl scripts
-```
-### cluster reads
-"$root"/bin/cluster.pl
-
-### split forward and reverse reads
-"$root"/bin/split.pl
-
-### extract barcodes and overhangs
-"$root"/bin/extract.pl
-
-### extract results
-"$root"/bin/reporter.pl
-
-### barcode table
-"$root"/bin/mktable_barcode.pl
-
-### result tables
-"$root"/bin/mktable.pl
-
-### optional
-"$root"/bin/split_overhangs_by_base.pl
-/mnt/home/potapov/projects/160202.LigaseFidelity/blunt_matrix_2.pl
-```
-
-## PacBio tools
-```
-samtools
-ccs
-blasr
-```
-
-
-
-## Timings
-```
-Cluster reads
-
-real    65m3.296s
-user    106m29.048s
-sys     5m41.955s
-```
-
-```
-Split reads
-
-real    54m20.970s
-user    78m1.393s
-sys     11m27.359s
-```
-
-```
-SAM-to-BAM
-
-real    15m20.012s
-user    13m40.762s
-sys     1m2.839s
-```
-# CP-LigaseFidelity -->
--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -2,140 +2,189 @@
 
 conda activate cp-ligfi
 
-### path to RADAR-seq repo
-# LIGFI_DIR="/PATH/TO/LIGFI_INSTALLATION_DIRECTORY"
-LIGFI_DIR="/mnt/home/potapov/projects/160202.LigaseFidelity/manuscript/github/ligase-fidelity/CP-LIGFI"
+### path to Ligase Fidelity repo
+# LIGASE_FIDELITY_DIR="/PATH/TO/LIGFI_INSTALLATION_DIRECTORY"
+LIGASE_FIDELITY_DIR="/mnt/home/potapov/projects/160202.LigaseFidelity/manuscript/github/ligase-fidelity/CP-LIGFI"
 
-export PATH=$LIGFI_DIR/bin:$PATH
+export PATH=$LIGASE_FIDELITY_DIR/bin:$PATH
 
-# ###############################################################################
-# ### Download example PacBio BAM subreads file                               ###
-# ###############################################################################
+###############################################################################
+### Download example PacBio BAM subreads file                               ###
+###############################################################################
 
 # wget https://sra-download.ncbi.nlm.nih.gov/traces/sra70/SRZ/019043/SRR19043774/lib26.bam
-# ln -s /mnt/pacbio/smrtlink/data/runs/64163/r64163_20220707_183853/4_D01/m64163_220709_012719.subreads.bam movie.subreads.bam
+ln -s /mnt/pacbio/smrtlink/data/runs/64163/r64163_20220707_183853/4_D01/m64163_220709_012719.subreads.bam movie.subreads.bam
 
-# ###############################################################################
-# ### Define input data files and output directory                            ###
-# ###############################################################################
+###############################################################################
+### Define input data files and output directory                            ###
+###############################################################################
 
 # input PacBio BAM subreads file
 subreads=$PWD/movie.subreads.bam
 
 # reference FASTA file
-reference=$LIGFI_DIR/references/b4.fasta
+reference=$LIGASE_FIDELITY_DIR/references/b4.fasta
 
 # output directory
-rundir=$PWD/ligfi_output
+rundir=$PWD/example/ligase_fidelity_output
 
 echo ""
 echo "subreads   : $subreads"
 echo "reference  : $reference"
 echo "output dir : $rundir"
 
-# ###############################################################################
-# ### Demultiplexing (optional)                                               ###
-# ###############################################################################
+###############################################################################
+### Cluster and split top/bottom strands                                    ###
+###############################################################################
 
-# echo ""
-# echo "Demultiplexing reads"
-
-# ### create output directory for demultiplexed reads
-# jobdir=$rundir/00-demux
-# mkdir -p $jobdir
-# cd $jobdir
-
-# ### demultiplex sequencing data
-# lima $subreads $barcodes movie.bam \
-#     --same \
-#     --split-bam \
-#     --num-threads 16 \
-#     --log-level TRACE \
-#     --log-file movie.log
-
-# ###############################################################################
-# ### Cluster top/bottom strands                                              ###
-# ###############################################################################
-
-# echo ""
-# echo "Cluster reads"
+echo ""
+echo "Cluster and split reads"
 
 ### create output directory for clustered reads
 jobdir=$rundir/01-cluster
 mkdir -p $jobdir
 cd $jobdir
 
-# time cluster.pl --insert_length 98 ${subreads} | bzip2 - > clusters.csv.bz2
+echo "cluster.pl"
+time cluster.pl --insert_length 98 ${subreads} | bzip2 - > clusters.csv.bz2
 
-# ###############################################################################
-# ### Split top/bottom strands                                                ###
-# ###############################################################################
+echo "split.pl"
+time split.pl ${subreads} clusters.csv.bz2
 
-# echo ""
-# echo "Split reads"
+echo "samtools (1)"
+time samtools view -Sb subreads.fwd.sam > subreads.fwd.bam
 
-# time split.pl ${subreads} clusters.csv.bz2
+echo "samtools (1)"
+time samtools view -Sb subreads.rev.sam > subreads.rev.bam
 
-# ###############################################################################
-# ### Build CCS sequences                                                     ###
-# ###############################################################################
+rm -f subreads.fwd.sam
+rm -f subreads.rev.sam
 
-# time samtools view -Sb subreads.fwd.sam > subreads.fwd.bam
+###############################################################################
+### Build CCS sequences                                                     ###
+###############################################################################
 
-# time ccs \
-#     --report-file=subreads_ccs.fwd.csv \
-#     --log-file=subreads_ccs.fwd.log \
-#     --num-threads=8 \
-#     --min-passes=1 \
-#     subreads.fwd.bam subreads_ccs.fwd.bam
+echo ""
+echo "Build CCS sequences"
 
-# time samtools view -Sb subreads.rev.sam > subreads.rev.bam
+### create output directory for consensus reads
+jobdir=$rundir/02-ccs
+mkdir -p $jobdir
+cd $jobdir
 
-# time ccs \
-#     --report-file=subreads_ccs.rev.csv \
-#     --log-file=subreads_ccs.rev.log \
-#     --num-threads=8 \
-#     --min-passes=1 \
-#     subreads.rev.bam subreads_ccs.rev.bam
+echo "ccs (1)"
+time ccs \
+    --report-file=subreads_ccs.fwd.csv \
+    --log-file=subreads_ccs.fwd.log \
+    --num-threads=8 \
+    --min-passes=1 \
+    $rundir/01-cluster/subreads.fwd.bam subreads_ccs.fwd.bam
 
-# time TMPDIR=$PWD pbmm2 align $reference subreads_ccs.fwd.bam aligned_reads.fwd.bam \
-#     --sort \
-#     --min-concordance-perc 75.0 \
-#     --sample \"\" \
-#     --num-threads 16 \
-#     --log-level TRACE \
-#     --log-file aligned_reads.fwd.log
+echo "ccs (2)"
+time ccs \
+    --report-file=subreads_ccs.rev.csv \
+    --log-file=subreads_ccs.rev.log \
+    --num-threads=8 \
+    --min-passes=1 \
+    $rundir/01-cluster/subreads.rev.bam subreads_ccs.rev.bam
 
-# time TMPDIR=$PWD pbmm2 align $reference subreads_ccs.rev.bam aligned_reads.rev.bam \
-#     --sort \
-#     --min-concordance-perc 75.0 \
-#     --sample \"\" \
-#     --num-threads 16 \
-#     --log-level TRACE \
-#     --log-file aligned_reads.rev.log
+###############################################################################
+### Map CCS sequences                                                       ###
+###############################################################################
 
-# time extract.pl \
-#     --region 1-3,3-10/1,10-12 \
-#     --region 45-47,47-52/1,52-54 \
-#     --region 87-89,89-96/1,96-98 \
-#     aligned_reads.fwd.bam $reference >fragments.fwd.csv 2>fragments.fwd.log
+echo ""
+echo "Map CCS sequences"
 
-# time extract.pl \
-#     --region 1-3,3-10/1,10-12 \
-#     --region 45-47,47-52/1,52-54 \
-#     --region 87-89,89-96/1,96-98 \
-#     aligned_reads.rev.bam $reference >fragments.rev.csv 2>fragments.rev.log
+### create output directory for mapped consensus reads
+jobdir=$rundir/03-mapping
+mkdir -p $jobdir
+cd $jobdir
 
-# time bam2csv.pl subreads_ccs.fwd.bam zmws.fwd.csv
-# time bam2csv.pl subreads_ccs.rev.bam zmws.rev.csv
+echo "pbmm2 (1)"
+time TMPDIR=$PWD pbmm2 align $reference $rundir/02-ccs/subreads_ccs.fwd.bam aligned_reads.fwd.bam \
+    --sort \
+    --min-concordance-perc 75.0 \
+    --sample \"\" \
+    --num-threads 16 \
+    --log-level TRACE \
+    --log-file aligned_reads.fwd.log
 
-# time reporter.pl \
-#     --etype b4 \
-#     --match 5 \
-#     --np 5 \
-#     --bcout barcodes.csv \
-#     --ohout overhangs.csv \
-#     .
+echo "pbmm2 (2)"
+time TMPDIR=$PWD pbmm2 align $reference $rundir/02-ccs/subreads_ccs.rev.bam aligned_reads.rev.bam \
+    --sort \
+    --min-concordance-perc 75.0 \
+    --sample \"\" \
+    --num-threads 16 \
+    --log-level TRACE \
+    --log-file aligned_reads.rev.log
 
-# mktable_barcode.pl barcodes.csv > table-barcode_base_frequencies.csv
-# mktable.pl --size 4 --prefix table overhangs.csv
+###############################################################################
+### Fragment CCS sequences                                                  ###
+###############################################################################
+
+echo ""
+echo "Fragment CCS sequences"
+
+### create output directory for fragmented consensus reads
+jobdir=$rundir/04-fragments
+mkdir -p $jobdir
+cd $jobdir
+
+echo "extract.pl (1)"
+time extract.pl \
+    --region 1-3,3-10/1,10-12 \
+    --region 45-47,47-52/1,52-54 \
+    --region 87-89,89-96/1,96-98 \
+    $rundir/03-mapping/aligned_reads.fwd.bam $reference >fragments.fwd.csv 2>fragments.fwd.log
+
+echo "extract.pl (2)"
+time extract.pl \
+    --region 1-3,3-10/1,10-12 \
+    --region 45-47,47-52/1,52-54 \
+    --region 87-89,89-96/1,96-98 \
+    $rundir/03-mapping/aligned_reads.rev.bam $reference >fragments.rev.csv 2>fragments.rev.log
+
+echo "bam2csv.pl (1)"
+time bam2csv.pl $rundir/02-ccs/subreads_ccs.fwd.bam zmws.fwd.csv
+
+echo "bam2csv.pl (2)"
+time bam2csv.pl $rundir/02-ccs/subreads_ccs.rev.bam zmws.rev.csv
+
+###############################################################################
+### Raw counts                                                              ###
+###############################################################################
+
+echo ""
+echo "Count overhangs"
+
+### create output directory for overhang counts
+jobdir=$rundir/05-results
+mkdir -p $jobdir
+cd $jobdir
+
+echo "reporter.pl"
+time reporter.pl \
+    --etype b4 \
+    --match 5 \
+    --np 5 \
+    --bcout barcodes.csv \
+    --ohout overhangs.csv \
+    $rundir/04-fragments
+
+###############################################################################
+### Summary tables                                                          ###
+###############################################################################
+
+echo ""
+echo "Tabulate results"
+
+### create output directory for summary tables
+jobdir=$rundir/06-summary
+mkdir -p $jobdir
+cd $jobdir
+
+echo "mktable_barcode.pl"
+mktable_barcode.pl $rundir/05-results/barcodes.csv > table-barcode_base_frequencies.csv
+
+echo "mktable.pl"
+mktable.pl --size 4 --prefix table $rundir/05-results/overhangs.csv
